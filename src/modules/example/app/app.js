d7547d86e0ac734f5345fbf479235a48efdc832b
Wes Taylor <wesley.taylor303@gmail.com>
2025-02-16 21:51:39 -0500

    start file input;

--- src/modules/example/app/app.js
+++ src/modules/example/app/app.js
@@ -1,5 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloWorldApp extends LightningElement {
-    testName = 'YEEZY';
+    @track fileName = '';
+    @track fileData;
+    @track uploadSuccess = false;
+    @track uploadError = false;
+
+    handleFileChange(event) {
+        const file = event.target.files[0];
+        console.error(JSON.stringify(file.size));
+        console.error(JSON.stringify(file.type));
+
+        if (file) {
+            this.fileName = file.name;
+            const reader = new FileReader();
+            reader.onload = () => {
+                this.fileData = reader.result; // Base64 encoded file data
+            };
+            reader.readAsDataURL(file); // This stores the file data in base64 format
+        }
+    }
+
+    handleUpload(){
+        console.error(JSON.stringify(this.fileData));
+        //console.error('refresh: ' + window.location.reload());
+    }
 }
